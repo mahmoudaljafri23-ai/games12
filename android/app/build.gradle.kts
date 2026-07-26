038bacf161b9a0e0f396defb72571e978f9160ca
Mahmoud Aljafri <mahmoud.aljafri23@gmail.com>
2026-07-26 11:37:23 +0300

    Updated applicationId to com.tahadi.almolook for Google Play Store

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.projectgoogl"
+        applicationId = "com.tahadi.almolook"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
